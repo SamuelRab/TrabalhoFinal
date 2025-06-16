895e63f0e2d18a84aebef8722980111a6292b7fb
SamuelRab <samuelrabelodasilva@gmail.com>
2025-06-16 16:20:52 -0300

    Update Entrega.js

--- Entrega.js
+++ Entrega.js
@@ -1,50 +1,62 @@
 const fs = require("fs");
 const promptsync = require("prompt-sync")();
 const path = require("path");
-const PromptSync = require("prompt-sync");
 
 let nome = []
 let endereco = []
 let distanciaEntrega = []
-let valorEntrega = 0;
+let valorEntrega = [];
 let tipoEntrega = []
+let i = 1;
+let normal = distanciaEntrega * valorEntrega;
+let urgente = (normal *0.2)+ normal;
+let entregasFeitas = [];
+let multiplicador = [];
 
 do {
 
-    nome = parseInt(PromptSync("Digite o seu nome:"));
+    nome = promptsync("Digite o seu nome:");
 
-    if (isNaN(nome)) {
-        alert("Inválido! Você não digitouum nome, escreva novamante!");
+    if (!isNaN(nome)) {
+        console.log("Inválido! Você não digitou um nome, escreva novamente!");
     }
 
-} while (isNaN(nome));
+    endereco = promptsync("Digite o seu endereço, como o seu bairro:");
 
-do {
-
-    endereco = parseInt(PromptSync("Digite o seu endereço, como o seu bairro:"));
-
-    if (isNaN(endereco)) {
-        alert("Inválido! Você não digitou o seu endereço, escreva novamente!");
+    if (!isNaN(endereco)) {
+        console.log("Inválido! Você não digitou o seu endereço, escreva novamente!");
     }
 
-} while (isNaN(endereco));
+    valorEntrega = parseInt(promptsync("Digite o valor da entrega:"));
 
-do {
+    if (isNaN(valorEntrega)) {
+        console.log("Inválido! Você não digitou um número, escreva novamente!");
+    }
 
-    distanciaEntrega = parseInt(PromptSync("Digite a distancia da entrega::"));
+    distanciaEntrega = parseInt (promptsync("Digite a distancia da entrega(em km):"));
 
     if (isNaN(distanciaEntrega)) {
-        alert("Inválido! Você não digitouum nome, escreva novamante!");
+        console.log("Inválido! Você não digitou um número, escreva novamente!");
     }
 
-} while (isNaN(distanciaEntrega));
+    tipoEntrega = promptsync("Qual o tipo da sua entrega? Se for normal, digite 1, se for urgente, digite 2:");
+
+    if (tipoEntrega = 1) {
+        console.log(distanciaEntrega * valorEntrega);
+    } else (tipoEntrega = 2); {
+        console.log((normal *0.2)+ normal)
+    }
+       
+    entregasFeitas = promptsync("você quer continuar as entregas? se sim, digite um número, se não quer digite uma letra:");
+
+    if (!isNaN(entregasFeitas)) {
+        console.log("Você decidiu continuar");
+    }
+
+    console.log(nome, endereco, distanciaEntrega, distanciaEntrega * valorEntrega)
+
+} while ( (!isNaN(nome)) || (!isNaN(endereco)) || (isNaN(valorEntrega)) || (isNaN(distanciaEntrega)) || (!isNaN(entregasFeitas)) || (!isNaN(tipoEntrega)));
 
 
 
-
-
-
-
-const normal = valorEntrega * distanciaEntrega 
-const urgente = valorEntrega * distanciaEntrega
 
